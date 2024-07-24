fb6ae771ef9e351d35bdb6b85f22af61616bfda1
Nandkishore Jatav <nandkishore.codewiseinfotech786@gmail.com>
2024-07-25 00:28:27 +0530

    conversion operation and conparision or string page complete

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -45,7 +45,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -58,14 +58,14 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
 // console.log("1" + 2);
-// console.log(1 + "2");
+// console.log(1 + "27");
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
-// console.log( (3 + 4) * 5 % 3);
+console.log( (3 + 4) * 5 % 3);
 
 // console.log(+true);
 // console.log(+"");
